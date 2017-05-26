fdaba985e548826055a1da0bcd0644870145cb50
Akshay Jain <akshayjainbu@gmail.com>
2017-05-26 19:00:06 -0400

    to find the friend circles in a matrix

--- graphs/dfs/FriendCircles.java
+++ graphs/dfs/FriendCircles.java
@@ -1,7 +1,13 @@
+/*Question from Leetcode-547 , Hackerrank
+There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. And we defined a friend circle is a group of students who are direct or indirect friends.
+
+Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not. And you have to output the total number of friend circles among all the students. 
+
+*/
+
 public class Solution {
     public int findCircleNum(int[][] M) {
         int n = M.length ;
-        
         boolean[] visited = new boolean[n];
         int friend_circles =0;
         
@@ -10,12 +16,9 @@ public class Solution {
             if ( !visited[i]){
                 findfriendsUtil( M, visited, i,n);
                 friend_circles ++;
-
-                
             }
         }
         return friend_circles;
-        
     }
     
     public void findfriendsUtil( int [][]M, boolean[] visited , int i , int n ){
