087084c70a317add3216121101a4211c9a3cfe9b
Akshay Jain <akshayjainbu@gmail.com>
2017-07-23 23:22:13 -0400

    Recover BST

--- BinaryTree/RecoverBST.java
+++ BinaryTree/RecoverBST.java
@@ -1,19 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package recoverbst;
-
-/**
- *
- * @author AkshayJain
- */
 public class RecoverBST {
 
     /**
      * @param args the command line arguments
      */
+
+    recoverBST(Node root){
+    if ( root == null)
+        return null;
+    
+    Node prev = null;
+    Node first = null;
+    Node last = null;
+    recoverBST( root.left);
+    if ( prev!= null){
+         if ( prev.data > root.data){
+                if ( first == null )
+                    first = prev;
+                last = root;
+                    }       
+        } 
+    prev = root;   
+    recoverBST(root.right);
+}
     public static void main(String[] args) {
         // TODO code application logic here
     }
